3c977c5f6ec6cd2d7f051887ec7cfa11dbfd1c83
201802170@o.cnu.ac.kr <201802170@o.cnu.ac.kr>
2022-12-29 17:38:40 +0900

    20221229

--- siameFC/SiameFC.py
+++ siameFC/SiameFC.py
@@ -6,74 +6,69 @@ from keras import backend as K
 from keras.layers import (
     Input,
     Lambda,
-    Conv2D,
-    ReLU, 
-    MaxPool2D, 
-    BatchNormalization
-    )
-import backbone.convolutional_alexnet as backbone
+)
+#import backbone.convolutional_alexnet as backbone
 
-Z_SHAPE = (127, 127, 3)
-X_SHAPE = (255, 255, 3)
+#test
+import backbone.alexnet_testpb as alexnet
 
-class SiameseFC(Model):
-    def __init__(self):
-        super(SiameseFC, self).__init__()
-        self.embedding = backbone()
+def x_corr_map(x):
 
-    def x_corr_map(x):
+    def _translation_match(i):  
+        x, z = i[0], i[1]
+        x = tf.expand_dims(x, 0)  
+        z = tf.expand_dims(z, -1)  
+        return tf.nn.conv2d(x, z, strides=[1, 1, 1, 1], padding='VALID', name='translation_match')
 
-        def _translation_match(i):  # translation match for one example within a batch
-            x, z = i[0], i[1]
-            x = tf.expand_dims(x, 0)  # [1, in_height, in_width, in_channels]
-            z = tf.expand_dims(z, -1)  # [filter_height, filter_width, in_channels, 1]
-            return tf.nn.conv2d(x, z, strides=[1, 1, 1, 1], padding='VALID', name='translation_match')
+    output = tf.map_fn(_translation_match, x, dtype=tf.float32)
+    output = tf.squeeze(output, [1, 4])
 
-        output = tf.map_fn(_translation_match, x, dtype=tf.float32)
-        output = tf.squeeze(output, [1, 4])  # of shape e.g., [8, 15, 15]
+    return output
 
+def x_corr_layer():
+    return Lambda(x_corr_map, output_shape=(17, 17))
     
-        return output
 
-    def x_corr_layer():
-        return Lambda(x_corr_map, output_shape=(17, 17))
-        
+def siameFc_model(x_shape, z_shape):
 
+    '''
+    exemplar = Input(shape=z_shape)
+    search = Input(shape=x_shape)
+    score_map = Input(shape=(17,17))
 
-    def Correlation(self, x, z):
-        return tf.squeeze(tf.nn.conv2d(x, z, [1,1,1,1], 'VALID'), 0)
+    embedding_exemplar = backbone.convolutional_alexnet(exemplar)
+    embedding_search = backbone.convolutional_alexnet(search)
 
-    def cross_correlation(inputs):
-        x = inputs[0]
-        x = tf.reshape(x, [1] + x.shape.as_list())
-        z = inputs[1]
-        z = tf.reshape(z, z.shape.as_list() + [1])
-        return tf.nn.convolution(x, z, padding='VALID', strides=(1,1))
+    score_map = x_corr_layer()([embedding_search, embedding_exemplar])
+    
+    outputs = [score_map]
+    inputs = [search, exemplar]
+    '''
+    
+    exemplar = Input(shape=z_shape)
+    search = Input(shape=x_shape)
+    
+    #model = Model(inputs=inputs, outputs=outputs)
 
-
-
-
-    def model(self, x_shape, z_shape):
-        exemplar = Input(shape=z_shape)
-        search = Input(shape=x_shape)
-        label_input = Input(shape=(17,17))
-
-        embedding_exemplar = backbone.convolutional_alexnet(exemplar).output
-        embedding_exemplar = backbone.convolutional_alexnet(search).output
+    alex_net = alexnet.alex_net_layers()
 
 
+    exemplar_features = alexnet.apply_layers(exemplar, alex_net)
+    search_features = alexnet.apply_layers(search, alex_net)
+    score_map = x_corr_layer()([search_features, exemplar_features])
 
-        
+    outputs = [score_map]
+    inputs = [search, exemplar]
 
-        score_map = x_corr_layer()([search_features, exemplar_features])
+    model = Model(inputs=inputs, outputs=outputs)
 
-        return Model(inputs=[exp, src], outputs=self.call([exp, src], False))
-
-
-        
+    return model
 
 
     
+
+
+
 
 
 
--- training/train.py
+++ training/train.py
@@ -1,10 +1,45 @@
-from loss.loss import loss_exp_fn
-from siameFC.SiameFC import SiameseFC
+import tensorflow as tf
+import loss.custom_loss as scoreMap_loss
+import siameFC.SiameFC as siameFc
+import numpy as np
+import warnings
+warnings.simplefilter(action='ignore', category=Warning)
 
-model = make_model(X_SHAPE, Z_SHAPE)
-model.compile(optimizer='adam', loss=loss_exp_fn, metrics=['accuracy'])
+from data_processing.data import make_label, load_images
 
-batch_size = 5
-epochs = 2
-model.fit([x_images, z_images], [labels], batch_size=batch_size, epochs=epochs)
 
+if __name__ == '__main__':
+    Z_SHAPE = (127, 127, 3)
+    X_SHAPE = (255, 255, 3)
+    LABEL = (17, 17, 1)
+
+    x_dir = "./sample/"
+    z_dir = "./sample/"
+    x_images = load_images(x_dir, 255, 0, ".x.jpg", normalize_images=True)
+    z_images = load_images(z_dir, 127, 0, ".z.jpg", normalize_images=True)
+
+    positive_label_pixel_radius = 16 # distance from center of target patch still considered a 'positive' match
+
+    response_size = 17
+    response_stride = 8.0
+
+    data_size = 1 # yet
+
+    label = make_label(response_size, positive_label_pixel_radius / response_stride)
+    labels = np.empty((data_size,) + label.shape)
+    labels[:] = label
+
+
+    model = siameFc.siameFc_model(X_SHAPE,Z_SHAPE)
+    model.summary()
+    
+    opt = tf.keras.optimizers.SGD(
+            momentum=0.9, nesterov=False, name='SGD'
+            )
+
+    model.compile(optimizer=opt, loss=scoreMap_loss, metrics=['accuracy'])
+
+
+    batch_size = 1 #yat
+    epochs = 1 #yat
+    model.fit([x_images, z_images], [labels], batch_size=batch_size, epochs=epochs)
