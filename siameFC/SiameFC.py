c186e44138468eb8e675e06fe8f1eded8391d8e1
sangho0804 <201802170@o.cnu.ac.kr>
2022-12-30 10:14:54 +0900

    test

--- siameFC/SiameFC.py
+++ siameFC/SiameFC.py
@@ -6,16 +6,22 @@ from keras.layers import (
 )
 import backbone.convolutional_alexnet as alexnet
 
-def x_corr_map(x):
-    exemplar = x[1]
-    search = x[0]
+def make_score_map(x):
 
-    output = tf.nn.conv2d(search, exemplar, strides=1, padding='VALID')
-    
+    def _translation_match(i):
+        x, z = i[0], i[1]
+        x = tf.expand_dims(x, 0)  
+        z = tf.expand_dims(z, -1)  
+        return tf.nn.conv2d(x, z, strides=1, padding='VALID')
+
+    output = tf.map_fn(_translation_match, x, dtype=tf.float32)
+
     return output
 
-def x_corr_layer():
-    return Lambda(x_corr_map, output_shape=(17, 17))
+
+
+def score_map_layer():
+    return Lambda(make_score_map, output_shape=(17, 17))
     
 
 def siameFc_model(x_shape, z_shape):
@@ -27,7 +33,7 @@ def siameFc_model(x_shape, z_shape):
     embedding_exemplar = alexnet.convolutional_alexnet(exemplar)
     embedding_search = alexnet.convolutional_alexnet(search)
 
-    score_map = x_corr_layer()([embedding_search, embedding_exemplar])
+    score_map = score_map_layer()([embedding_search, embedding_exemplar])
     
     outputs = [score_map]
     inputs = [search, exemplar]
