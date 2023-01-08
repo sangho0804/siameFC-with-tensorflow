d00c51e291c22cd5beadcf5093fb81923dc44208
201802170@o.cnu.ac.kr <201802170@o.cnu.ac.kr>
2023-01-08 22:55:21 +0900

    2023 01 08

--- siameFC/loss_of_scoreMap.py
+++ siameFC/loss_of_scoreMap.py
@@ -5,6 +5,6 @@ def loss_of_scoreMap(y_true, y_pred):
     product = -y_true * y_pred
     probs = 1 + K.clip(K.exp(product), 0, 1e6)
     loss = K.log(probs + 1e-7)
-    return K.mean(loss, axis=(1,2))
+    return K.mean(loss)
 
 
